296d61fb15954910bbe3b7ec713dbaa9843d0502
Daniel <daniel31x13@gmail.com>
2022-05-14 18:05:20 +0430

    Small changes

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ function startProgressBar(amount) {
         clearInterval(id);
         i = 0;
       } else {
-        width = width+5;
+        width = width+10;
         elem.style.width = width + "%";
         elem.innerHTML = width  + "%";
       }
